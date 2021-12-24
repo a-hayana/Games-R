b1efcbd1ec3b95d2e32eb7a805bbf48ba85af59c
Ariane Hayana <economista.ariane@gmail.com>
2021-12-23 23:57:37 -0400

    Initial commit

---
+++ Games-R.R
@@ -0,0 +1,16 @@
+# install.packages("devtools")
+# devtools::install_github('RLesur/Rcade')
+
+# Lista com os jogos
+Rcade::games
+
+# PacMan
+Rcade::games$Pacman
+
+# Super Mario
+Rcade::games$Mariohtml5
+
+
+# Detalhes em: https://estatisticablog.netlify.app/post/jogos-no-r-sim/
+
+
